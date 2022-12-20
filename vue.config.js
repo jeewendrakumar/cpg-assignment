2356f927a4789283baa19b573efafcaa27527609
jeewendra kumar <jeewendrakumar@gmail.com>
2022-12-20 23:16:09 +0100

    commit files

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     config.resolve.alias.set(
       'vue$',
       // If using the runtime only build
-      path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
+      path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm-bundler.js')
       // Or if using full build of Vue (runtime + compiler)
       // path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
     )
